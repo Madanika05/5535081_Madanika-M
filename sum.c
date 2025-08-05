886852bc579b482fcd20d663da9c28c6398c83c1
Madanika <madanika901@gmail.com>
2025-08-05 11:33:04 +0530

    Deleted sample.txt, sed_test, sum.c, and test.c

--- sum.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-	int a,b,c;
-	a=15;
-	b=30;
-	c=a+b;
-	printf("sum=%d",c);
-	return 0;
-}
--- test.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
- printf("welcome to Linux\n");
- int a=5,b=4,c;
- c=a+b;
- printf("sum = %d\n", c);
- return 0;
-}
